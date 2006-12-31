9130699d8643e90987e0ecb6a401c7158733cd72
palmnet <palmnet@07406f30-5f1d-0410-8162-b1cbff782bc3>
2006-12-31 19:52:21 +0000

    Added a folder remotely

---
+++ Source_files/Release_0_5/shoutbox/config.php
@@ -0,0 +1,16 @@
+<?php
+	//Admin details
+	$adminname = "Admin";
+	$adminpassword = "+pass";
+	
+	//The location of the content file
+	$contentfile = "content.txt";
+	
+	//Shoutbox options
+	$rssitemcount = 25;
+	$timestamps = false;
+	
+	//Position of the shoutbox area on the shoutbox
+	$Xval = 8;
+	$Yval = 35;
+?>
---
+++ Source_files/Release_0_5/shoutbox/image.php
@@ -0,0 +1,53 @@
+<?php
+//The image.php file is the one that outputs the shoutbox image.
+// This is what you link to when using ths shoutbox on various sites.
+//
+//Include the config.php because we need some variables from it
+include "config.php";
+
+//Set the header to make the browser display this as a gif image
+header ('Content-Type: image/gif');
+
+//Open the file with the content in so we can read it and display it on the shoutbox image
+$lines = str_replace("\n", '', file($contentfile));
+$lines = str_replace("\r", '', $lines);
+
+//This code shows the timestamps or not, dependant on the config.php
+if(!$timestamps) {
+	$lines = preg_replace("/\[[0-9][0-9]:[0-9][0-9](am|pm)\]/","",$lines);
+}
+
+//Load the images into variables
+$image = imagecreatefromjpeg('design/background.jpg');
+$sigimg = imagecreatefromjpeg('design/shoutbox.jpg');
+
+//RGB colour of the text:
+$text_color = imagecolorallocate($image, 55, 55, 55);
+
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!Do not mess with the code below this line unless you know what you're doing!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//The value of Y is amount of full lines of text we can squeeze into the shutbox image
+$y = (floor(imagesy($image)/imagefontheight(2)) * imagefontheight(2)) - imagefontheight(2);
+// Rydian Morrison is responsible for the following loop, It's ace, word wraps and draws every line :D
+for ($i = 0; $i < count($lines); $i++)
+{
+	//Indent idea courtesy of Hoggs
+	$text = wordwrap($lines[$i], floor((imagesx($image)-11)/imagefontwidth(2)), "\n  ", 1);
+	$text = explode("\n", $text);
+	
+	for ($j = (count($text) - 1); $j >= 0; $j--)
+	{
+		imagestring($image, 2, 7, $y, $text[$j], $text_color);
+		$y -= imagefontheight(2);
+	}
+}
+
+//Copy the shoutbox part of the image onto the main shoutbox image
+imagecopyresampled($sigimg, $image, $Xval, $Yval, 0, 0, imagesx($image), imagesy($image), imagesx($image), imagesy($image));
+
+//Output the final image to the user
+imagegif($sigimg);
+?>
