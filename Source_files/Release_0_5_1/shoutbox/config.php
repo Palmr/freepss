c3fe074b69ca80184a25015114186282316917b1
palmnet <palmnet@07406f30-5f1d-0410-8162-b1cbff782bc3>
2007-01-02 01:28:30 +0000

    Added a file remotely

---
+++ Source_files/Release_0_5_1/shoutbox/config.php
@@ -0,0 +1,18 @@
+<?php
+	//Admin details
+	$adminname = "Admin";
+	$adminpassword = "+pass";
+	
+	//The location of the content file
+	$contentfile = "content.txt";
+	
+	//Shoutbox options
+	$rssitemcount = 25;
+	$timestamps = false;
+	
+	//Shoutbox image options
+	$shoutboximg = "design/background.jpg";
+	$shoutboxbgimg = "design/shoutbox.jpg";
+	$Xval = 8;
+	$Yval = 35;
+?>
